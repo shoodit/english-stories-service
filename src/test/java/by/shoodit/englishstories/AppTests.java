f626082917ff12861238784a653db73da71e44fa
shoodit <vi.majeure@gmail.com>
2022-03-26 16:21:58 +0300

    Initial commit

---
+++ src/test/java/by/shoodit/englishstories/AppTests.java
@@ -0,0 +1,13 @@
+package by.shoodit.englishstories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
